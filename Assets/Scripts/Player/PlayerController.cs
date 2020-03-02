7a942814bc501347b11acd4f49698ad7eb0c81bc
Geet Chopra <geet.s.chopra@gmail.com>
2020-03-02 00:27:25 -0500

    Gradual increase of player movement speed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed;
     private Vector3 direction;
     private CharacterController controller;
+    private Animator animator;
 
     /// <summary>
     /// Start - Called before the first frame update.
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         sprintSpeed = walkSpeed * 2.0f;
+        animator = GetComponent<Animator>();
     }
 
     /// <summary>
@@ -28,6 +31,9 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     void FixedUpdate()
     {
+        animator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+        animator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+
         //Set the move speed based on whether the player is sprinting or not.
         moveSpeed = Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : walkSpeed;
         
@@ -36,31 +42,29 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             direction = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
-            controller.Move(direction * Time.deltaTime * moveSpeed);
+            controller.Move(direction * Time.deltaTime * moveSpeed * Math.Abs(animator.GetFloat("VSpeed")));
         }
         
         if (Input.GetKey(KeyCode.S))
         {
             direction = new Vector3(-Camera.main.transform.forward.x, 0, -Camera.main.transform.forward.z);
-            controller.Move(direction * Time.deltaTime * moveSpeed);
+            controller.Move(direction * Time.deltaTime * moveSpeed * Math.Abs(animator.GetFloat("VSpeed")));
         }
         
         if (Input.GetKey(KeyCode.D))
         {
             direction = new Vector3(Camera.main.transform.right.x, 0, Camera.main.transform.right.z);
             moveSpeed /= 2.0f;
-            controller.Move(direction * Time.deltaTime * moveSpeed);
+            controller.Move(direction * Time.deltaTime * moveSpeed * Math.Abs(animator.GetFloat("HSpeed")));
         }
         
         if (Input.GetKey(KeyCode.A))
         {
             direction = new Vector3(-Camera.main.transform.right.x, 0, -Camera.main.transform.right.z);
             moveSpeed /= 2.0f;
-            controller.Move(direction * Time.deltaTime * moveSpeed);
+            controller.Move(direction * Time.deltaTime * moveSpeed * Math.Abs(animator.GetFloat("HSpeed")));
         }
 
-        //Update player body to be looking in the correct direction.
-        if (direction != Vector3.zero)
-            transform.forward = direction;
+        transform.forward = new Vector3(Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z);
     }
 }
